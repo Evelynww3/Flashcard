7c009eb90539f87e157293fbcf9c295164a250df
Evelynww3 <evelyn.camargo.sampaio@escola.pr.gov.br>
2025-08-11 08:22:02 -0300

    e

---
+++ assets/pergunta.js
@@ -0,0 +1,25 @@
+
+criaCartao(
+    'Doença de todd',
+    'O que é essa doença?',
+    'É uma paralisia temporária após uma convulsão, geralmente de um lado do corpo.'
+)
+
+criaCartao(
+    'Sintomas',
+    'Quais são os sintomas?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Precauções',
+    'Pode ser evitada?,
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Tratamento',
+    'Como é tratada?',
+    ' Não precisa de tratamento específico, só controlar as convulsões.
+
+)
